1166c1c23bd1448b59d93ff50e9e2451073a1f4c
Spencer Davis <spencerdavis2012@gmail.com>
2017-09-11 04:24:52 -0500

    FIXED THE FUCKING EVERYTHING

---
+++ src/wdv240/project1/wp-content/themes/Neddy/footer.php
@@ -0,0 +1,13 @@
+  <div class="span-24">
+		<div id="footer">Copyright &copy; <a href="<?php bloginfo('home'); ?>"><strong><?php bloginfo('name'); ?></strong></a>  - <?php bloginfo('description'); ?></div>
+        
+        <div id="credits">Powered by <a href="http://wordpress.org/"><strong>WordPress</strong></a></div>
+    </div>
+</div>
+</div>
+<?php
+	 wp_footer();
+	echo get_theme_option("footer")  . "\n";
+?>
+</body>
+</html>
---
+++ src/wdv240/project1/wp-content/themes/Neddy/readme.txt
@@ -0,0 +1,3 @@
+Neddy theme by NewWpThemes, http://newwpthemes.com
+Online Demo: http://newwpthemes.com/demo/Neddy/
+Theme URI: http://newwpthemes.com/neddy-free-wordpress-theme/
