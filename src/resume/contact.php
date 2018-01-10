363d13323602c9d880d427eaa3d59104c8c70fbc
Spencer Davis <spencerdavis2012@gmail.com>
2018-01-10 14:46:36 -0600

    fixed all the things for the resume and portfolio project

---
+++ src/resume/contact.php
@@ -0,0 +1,256 @@
+<html lang="en">
+
+<head>
+  <title>Spencer Davis</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous" />
+  <link rel="stylesheet" href="/shards.min.css" />
+  <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+  <link rel="shortcut icon" href="/images/slogo.png" />
+  <link rel="stylesheet" href="/css/main.css" />
+
+</head>
+
+<body>
+
+  <!--Nav menu -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <a class="navbar-brand" href="/">Spencer Davis</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item ">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/resume/index.html">Resume<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact-form">Contact Me</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Homework
+        </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="/WDV/Homework/wdv101homework.html">Intro HTML/CSS (2017)</a>
+            <a class="dropdown-item" href="/wdv205/index.html">Adv CSS (2017)</a>
+            <a class="dropdown-item" href="/introJavaScript/index.html">Intro Javascript (2017)</a>
+          </div>
+        </li>
+        <li>
+          <a class="gitgrey d-sm-block d-lg-none d-xl-none" href="https://github.com/instagibby/sldavis"><i class="fab fa-2x fa-github"></i></a>
+        </li>
+      </ul>
+    </div>
+    <a class="gitgrey hidesmall d-sm-none d-xl-block" href="https://github.com/instagibby/sldavis"><i class="fab fa-2x fa-github"></i></a>
+  </nav>
+  <!-- nav menu end --->
+
+
+
+
+  <div class="container col-lg-9">
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="alert alert-success" role="alert">
+          Thanks for reaching out to me, I'll get back to you as soon as possible!
+          </div>
+          <div class="container">
+            <h1 class="display-3">Spencer Davis</h1>
+            <h4 class="leftalign">Employment</h4>
+            <p class="lead leftalign">
+              <strong>Experience Specialist/Solutions Specialist, December 2015-Present Verizon Wireless, Ankeny, IA.</strong></br>
+              In charge of handling sensitive customer information as well as cash transactions.</br>
+              Knowledgeable about current promotions, plan and phone pricing.</br>
+              Highest rated store employee for the customer satisfaction survey.</br>
+              Capable of troubleshooting and diagnosing technical issues on mobile devices.</br>
+              <p class="lead leftalign">
+                <strong>Server/Bartender, August 2011 - December 2015 The Spice: Thai Cuisine, Ames, IA.</strong></br>
+                Trained new employees on their tasks and responsibilities.</br>
+                Had access to manager computer code to assist co workers’ problems when boss is occupied.</br>
+                Asked to assist in manager duties.</br>
+                Thorough understanding of food preparation, cooking, storing, and restocking processes.</br>
+                Learning and knowing the ins and outs of the dishes and sauces we sell has helped develop attention to detail.
+              </p>
+              <h4>Education</h4>
+              <p class="lead leftalign">
+                Des Moines Area Community College, Ankeny, IA. 2016 - Present</p>
+              <p class="lead leftalign" style="text-indent: 10%;">Liberal Arts (Pursuing)</p>
+              <p class="lead leftalign" style="text-indent: 10%;">Web Dev Diploma (Pursuing)</p>
+              <p class="lead leftalign">Ames High School, Ames, IA, graduated 2012
+
+              </p>
+              <h4 class="leftalign">Employment</h4>
+              <h6 class="leftalign">Excel in Customer Service, Leadership, Time Management, Flexibility, Detail Orientation,  Technology;</h6>
+              <p class="lead leftalign">
+                Strive to meet customers in an efficient and friendly manner.</br>
+                Repeatedly assist other employee’s customers in understanding and handling situations.</br>
+                Working in the customer service/sales industry requires focus and haste. The customer must know about all of the promotions as well as get the products they need in an efficient manner.</br>
+                Small restaurants require everyone to work as quickly and precisely as possible. I’ve had several years of practice to make sure everyone has what they need and nobody has to wait for me to get them assistance.</br>
+                Certified in Microsoft: Word, Excel, and Powerpoint. As well as Google docs, Google Spreadsheet, and Google Slides.</br>
+                Written over thirty thousand lines of code to create web apps as well as phone apps.</br>
+                Capable of coding in: Javascript, Python, c# and able to use wordpress.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-10 offset-lg-1">
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-3">Contact Me</h1>
+
+            <form id="contact-form" method="post" action="contact.php" role="form">
+
+              <div class="messages"></div>
+
+              <div class="controls">
+
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="form_name">First name *</label>
+                      <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required.">
+                      <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="form_lastname">Last name *</label>
+                      <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required.">
+                      <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="form_email">Email *</label>
+                      <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+                      <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="form_phone">Phone</label>
+                      <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+                      <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="form_message">Message*</label>
+                      <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                      <div class="help-block with-errors"></div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <input type="submit" class="btn btn-dark btn-send" value="Send message">
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+  <?php
+  /*
+   *  CONFIGURE EVERYTHING HERE
+   */
+
+  // an email address that will be in the From field of the email.
+  $from = 'webdev@sldavis.info';
+
+  // an email address that will receive the email with the output of the form
+  $sendTo = 'spencerdavis2012@gmail.com';
+
+  // subject of the email
+  $subject = 'New message from contact form';
+
+  // form field names and their translations.
+  // array variable name => Text to appear in the email
+  $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+
+  // message that will be displayed when everything is OK :)
+  $okMessage = '';
+
+  // If something goes wrong, we will display this message.
+  $errorMessage = 'There was an error while submitting the form. Please try again later';
+
+  /*
+   *  LET'S DO THE SENDING
+   */
+
+  // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+  error_reporting(0);
+
+  try
+  {
+
+      if(count($_POST) == 0) throw new \Exception('Form is empty');
+
+      $emailText = "You have a new message from your contact form\n=============================\n";
+
+      foreach ($_POST as $key => $value) {
+          // If the field exists in the $fields array, include it in the email
+          if (isset($fields[$key])) {
+              $emailText .= "$fields[$key]: $value\n";
+          }
+      }
+
+      // All the neccessary headers for the email.
+      $headers = array('Content-Type: text/plain; charset="UTF-8";',
+          'From: ' . $from,
+          'Reply-To: ' . $from,
+          'Return-Path: ' . $from,
+      );
+
+      // Send email
+      mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+      $responseArray = array('type' => 'success', 'message' => $okMessage);
+  }
+  catch (\Exception $e)
+  {
+      $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+  }
+
+
+  // if requested by AJAX request return JSON response
+  if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+      $encoded = json_encode($responseArray);
+
+      header('Content-Type: application/json');
+
+      echo $encoded;
+  }
+  // else just display the message
+  else {
+      echo $responseArray['message'];
+  }
+?>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <script src="/wdv205/portfolio/js/shards.min.js"></script>
+
+</body>
+</html>
--- src/wdv240/project1/wp-content/themes/Neddy/readme.txt
+++
@@ -1,3 +0,0 @@
-Neddy theme by NewWpThemes, http://newwpthemes.com
-Online Demo: http://newwpthemes.com/demo/Neddy/
-Theme URI: http://newwpthemes.com/neddy-free-wordpress-theme/
