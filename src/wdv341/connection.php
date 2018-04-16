bfafd2645fdba9b0cf4ec9196da809b44bf297c5
Spencer Davis <spencerdavis2012@gmail.com>
2018-04-16 02:13:28 -0500

    php SQL connection page working

---
+++ src/wdv341/connection.php
@@ -0,0 +1,21 @@
+<?php
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$database = "wdv341";
+
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<p>Connected successfully</p>";
+
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+
+?>
